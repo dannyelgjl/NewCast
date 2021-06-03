a0ab4234bded9a84e501ec583a1a914576270f5a
Daniel Jeronimo <daniel.leite@midway.com.br>
2021-06-03 02:32:34 -0300

    feat: configurando navegacao

---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from '../pages/Home';
+
+const HomeNavigation = createStackNavigator();
+
+
+export default function App() {
+  return (
+    <HomeNavigation.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: { backgroundColor: '#fff' }
+      }}
+    >
+      <HomeNavigation.Screen name="Home" component={Home} />
+    </HomeNavigation.Navigator>
+  );
+}
