2da365fb0a17bf0e01fe7d254b20a5fc8f70dff2
Daniel Jeronimo <daniel.leite@midway.com.br>
2021-06-03 22:55:14 -0300

    feat: criando component verticallist

---
+++ src/components/VerticalList/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Title, FlatCard } from '../';
+import { Container, Content } from './styles';
+
+interface Props {
+  title?: string;
+  data: {
+    id: string;
+    title: string;
+    desc: string;
+    thumbnail: string;
+    category: string;
+  }[],
+  item?: {
+    id?: string;
+  };
+}
+
+const VerticalList: React.FC<Props> = ({ title, data }) => {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Content>
+        {data.map(item => <FlatCard item={item} key={item.id} />)}
+      </Content>
+    </Container>
+  );
+}
+
+export default VerticalList;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -21,8 +21,8 @@ const News = () => {
       <BreakingNews data={breakingNews} />
       <TechNews data={techNews} />
       <FlatCard item={{
-        id: "1",
-        title: "Teste titulo 1 2 3",
+        id: "4",
+        title: "Teste titulo 1 2 5",
         desc:
           "desad asd edasd deasd deasd dasda dwasdas dwasd.",
         thumbnail: "https://i.pinimg.com/originals/70/02/6e/70026eec5353580f50ea89127ff895e7.png",
